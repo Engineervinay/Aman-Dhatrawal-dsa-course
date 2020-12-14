03c86b8666ef27a8db37377a31b4e97db33f144c
Vinay Patil <vinay1645.vp@gmail.com>
2020-12-14 17:45:19 +0530

    Add files via upload

---
+++ 7.pairSum.cpp
@@ -0,0 +1,62 @@
+/*********************************
+github repository:
+https://github.com/Engineervinay/Placement-Preparation-Course-cpp
+*********************************/
+
+
+#include <iostream>
+//pair sum is equal to given number 
+using namespace std;
+
+int main() {
+    int n,number,i,j;
+    int a[n];
+    cin>>n>>number;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+        
+    }
+    int res=0;
+    
+    
+    //brute force approach time complexity n^2
+    /*
+    for(i =0;i<n-1;i++){
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]+a[j]==number)
+            {
+                res=1;
+                break;
+                
+            }
+        }
+    }
+    */
+    
+    
+    //for sorted array:
+    // using optimized approch like binary search time complexity o(n)
+    
+    int low=0,high=n-1;
+    while(low<high)
+    {
+        if(a[low]+a[high]==number)
+        {
+            res=1;
+            break;
+        }
+        else if(a[low]+a[high]<number)
+        {
+            low++;
+        }
+        else {
+            high--;
+        }
+    }
+    
+    
+    (res==0)? cout<<"not present" : cout<<"present";
+    
+}
