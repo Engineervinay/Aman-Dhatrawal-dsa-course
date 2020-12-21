61e6491b788c415d12e60735844f3231580a1b09
Vinay Patil <vinay1645.vp@gmail.com>
2020-12-21 19:08:23 +0530

    Add files via upload

---
+++ 17.subaraysSum.cpp
@@ -0,0 +1,31 @@
+/*********************************
+github repository:
+https://github.com/Engineervinay/Placement-Preparation-Course-cpp
+*********************************/
+
+
+#include <iostream>
+
+using namespace std;
+//subarrays
+
+int main() {
+    int n,i,j,a[10];
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+        
+    }
+    for(i=0;i<n;i++)
+    {
+        int sum=0;
+        for(j=i;j<n;j++)
+        {
+            sum+=a[j];
+            
+        }
+	cout<<sum<<endl;
+    }
+    
+}
