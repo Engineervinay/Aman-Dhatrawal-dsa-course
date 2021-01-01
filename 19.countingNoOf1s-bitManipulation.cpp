abe6ebb15ffc8ed05a6338b62aa7f70c818eabe2
Vinay Patil <vinay1645.vp@gmail.com>
2021-01-01 16:38:06 +0530

    Add files via upload

---
+++ 19.countingNoOf1s-bitManipulation.cpp
@@ -0,0 +1,21 @@
+/*********************************
+github repository:
+https://github.com/Engineervinay/Placement-Preparation-Course-cpp
+*********************************/
+
+#include <iostream>
+//bit manipulation - counting number of one's
+using namespace std;
+
+int main() {
+    //do a=a&a-1 till a becomes 0
+    int a;
+    cin>>a;
+    int count=0;
+    while(a!=0)
+    {
+        a=a&a-1;
+        count++;
+    }
+    cout<<count;
+}
